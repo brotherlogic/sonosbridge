a97111771dd1937db7d6e177685f39f35fcc056e
Simon Tucker <brotherlogic@gmail.com>
2022-11-02 16:39:13 -0700

    Save token first

--- sonosbridgeapi.go
+++ sonosbridgeapi.go
@@ -105,6 +105,7 @@ func (s *Server) SetConfig(ctx context.Context, req *pb.SetConfigRequest) (*pb.S
 	}
 	if len(req.GetCode()) > 0 {
 		config.Code = req.GetCode()
+		s.saveConfig(ctx, config)
 		_, err := s.GetToken(ctx, &pb.GetTokenRequest{})
 		return &pb.SetConfigResponse{}, err
 	}
