6ff7dbdd9aa6bf57a20f3abe12f14f75c09c4f45
Simon Tucker <brotherlogic@gmail.com>
2022-11-03 14:10:33 -0700

    Working on get household

---
+++ sonosbridgeutils.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/net/context"
+)
+
+func (s *Server) runGet(ctx context.Context, host, path, token string) ([]byte, error) {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("https://%v%v", host, path), nil)
+	res, err := s.hclient.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		defer res.Body.Close()
+		body, _ := ioutil.ReadAll(res.Body)
+		s.CtxLog(ctx, fmt.Sprintf("BODY: %v", string(body)))
+		return nil, fmt.Errorf("Bad response on token retrieve:(%v) %v", res.StatusCode, res)
+	}
+
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+
+	return body, nil
+}
