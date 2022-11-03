4f9a9b2e1236adafb53c235393cab57b4d44a634
Simon Tucker <brotherlogic@gmail.com>
2022-11-03 14:39:58 -0700

    Added runGet tests

---
+++ sonosbridgeutils_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestBadRequest(t *testing.T) {
+	s := GetTestServer()
+	s.hclient = &testClient{failure: fmt.Errorf("built to fail")}
+
+	res, err := s.runGet(context.Background(), "", "", "")
+	if err == nil {
+		t.Errorf("Should have failed: %v", res)
+	}
+}
+
+func TestErrorCode(t *testing.T) {
+	s := GetTestServer()
+	s.hclient = &testClient{responseCode: 400}
+
+	res, err := s.runGet(context.Background(), "", "", "")
+	if err == nil {
+		t.Errorf("Should have failed: %v", res)
+	}
+}
